8c52daf5e091aa45992656218b83ee6014ebe8fb
Zane Pollard <zane.pollard@thesitecontroller.local>
2018-07-05 13:35:29 -0500

    testing

--- boibot/boibot.js
+++ boibot/boibot.js
@@ -1,16 +1,19 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
-//Bot token goes here
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 
 client.on('ready', () => {
     console.log('I am ready!');
   });
 
 client.on('message', message => {
-    
-  });
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  client.login(token);
+  const args = message.content.slice(prefix.length).split(' ');
+  const command = args.shift().toLowerCase();
+  if (message.content.startsWith(`${prefix}test`)) {
+    message.channel.send(`Responding to test\nHave a nice day ${message.author.username}.`);
+  }
+});
+
+client.login(token);
