4b7bf1fdf5609506497041bd7416951e5a65fbfd
caseycarroll <aiyakc@gmail.com>
2018-07-11 12:46:11 -0500

    improve help unit tests

--- commands/help.js
+++ commands/help.js
@@ -9,16 +9,14 @@ const helpProperties = {
 
 const get = (message, commands, args) => {
     const data = [];
-    console.log(args[0])
     const command = commands.find(command => command.aliases.find( alias => alias === args[0]));
-    console.log("command: ", command)
     
     if (!args.length || command === undefined) {
         data.push('Here\'s a list of all my commands:');
         data.push(commands.map(command => command.name).join(', '));
         data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-        return message.author.send(data, { split: true })
+        message.author.send(data, { split: true })
             .then(() => {
                 if (message.channel.type === 'dm') return;
                 message.reply('I\'ve sent you a DM with all my commands!');
@@ -27,6 +25,7 @@ const get = (message, commands, args) => {
                 console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
                 message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
             });
+        return;
     }
 
     data.push(`**Name:** ${command.name}`);
@@ -35,7 +34,8 @@ const get = (message, commands, args) => {
     if (command.description) data.push(`**Description:** ${command.description}`);
     if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
-    return message.channel.send(data, { split: true });
+    message.channel.send(data, { split: true });
+    return command;
 }
 
 module.exports = {
--- tests/help.spec.js
+++ tests/help.spec.js
@@ -5,11 +5,15 @@ const dummyMessage = require('./dummy-message');
 
 const commandProperties = [help.properties, nickName.properties];
 describe("Send Help", function () {
-    it("Responds to help request with arguments", function () {
-        dummyArguments = ["1234"]
-        assert.notEqual(help.get(dummyMessage, commandProperties, dummyArguments), undefined);
-    })
+    it("Responds to help request with real argument", function () {
+        dummyArguments = ["n"]
+        assert.deepEqual(help.get(dummyMessage, commandProperties, dummyArguments), nickName.properties);
+    });
+    it("Responds to help request with many arguments", function () {
+        dummyArguments = ["1234", "asdfas", "asdfjj"]
+        assert.equal(help.get(dummyMessage, commandProperties, dummyArguments), undefined);
+    });
     it("Responds to help request with no arguments", function () {
-        assert.notEqual(help.get(dummyMessage, commandProperties, dummyArguments), undefined);
-    })
+        assert.equal(help.get(dummyMessage, commandProperties, dummyArguments), undefined);
+    });
 });
