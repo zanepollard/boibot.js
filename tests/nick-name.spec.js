3201ef1d37d981223086f1f2612af93e6bdf50ae
caseycarroll <aiyakc@gmail.com>
2018-07-12 22:20:16 -0500

    add error handling for empty arguments in nickname changer

--- tests/nick-name.spec.js
+++ tests/nick-name.spec.js
@@ -3,11 +3,15 @@ const nickName = require("../commands/nick-name");
 const dummyMessage = require("./dummy-message");
 
 const testNickname = "Testing 3234$$@#$";
-const testCommand = ["@someUserName", "Testing", "3234$$@#$"];
+let testArguments = ["@someUserName", "Testing", "3234$$@#$"];
 
 describe("Change User's Name", function() {
   it("Sets the username correctly", function(done) {
-    assert.equal(nickName.change(dummyMessage, testCommand), testNickname);
+    assert.equal(nickName.change(dummyMessage, testArguments), testNickname);
     done();
   });
+  it("cancels command when no arguments are passed", function () {
+    testArguments = [" "];
+    assert.equal(nickName.change(dummyMessage, testArguments), dummyMessage.mentions.users.first().username);
+  })
 });
